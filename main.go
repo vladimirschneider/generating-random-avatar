19befd01df7c3eccc18aceea6999408c820751ff
vladimirschneider <42021006+vladimirschneider@users.noreply.github.com>
2021-01-20 11:46:16 +0700

    Init

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const avatarSize = 1000
+
+const brushSize = 50
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	avatar := image.NewRGBA(image.Rect(0, 0, avatarSize, avatarSize))
+	PainBG(avatar, GenerateBGColor())
+	PaintAvatar(avatar, GenerateAvatarColor())
+	SavePNG(avatar)
+}
+
+func SavePNG(avatar image.Image) {
+	file, err := os.Create("avatar.png")
+	err  = png.Encode(file, avatar)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func PaintAvatar(avatar *image.RGBA, avatarColor color.RGBA) {
+	for x := 0; x < avatarSize / 2; x += brushSize {
+		for y := 0; y < avatarSize; y += brushSize {
+			if rand.Float32() > 0.5 {
+				for rectX := x; rectX <= x + brushSize; rectX++ {
+					for rectY := y; rectY <= y + brushSize; rectY++ {
+						avatar.SetRGBA(rectX, rectY, avatarColor)
+						avatar.SetRGBA(avatarSize - rectX, rectY, avatarColor)
+					}
+				}
+			}
+		}
+	}
+}
+
+func PainBG(avatar *image.RGBA, bgColor color.RGBA) {
+	for y := 0; y < avatarSize; y++ {
+		for x := 0; x < avatarSize; x++ {
+			avatar.SetRGBA(x, y, bgColor)
+		}
+	}
+}
+
+func GenerateAvatarColor() (avatarColor color.RGBA) {
+	avatarColor = GenerateColor(50, 100)
+	return
+}
+
+func GenerateBGColor() (bgColor color.RGBA) {
+	bgColor = GenerateColor(200, 255)
+	return
+}
+
+func GenerateColor(min, max float32) (color color.RGBA) {
+	color.A = 255
+
+	color.R = uint8((rand.Float32() * (max - min)) + min)
+	color.G = uint8((rand.Float32() * (max - min)) + min)
+	color.B = uint8((rand.Float32() * (max - min)) + min)
+
+	return
+}
